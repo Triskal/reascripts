7be47d9791a648806876a8d7cfc779331ff803ea
Simon Kok <simon.j.kok@gmail.com>
2024-11-09 19:09:20 +0200

    v2.0

--- Stretch markers/SimonJ_Convert stretch markers to splits.lua
+++ Stretch markers/SimonJ_Convert stretch markers to splits.lua
@@ -4,7 +4,11 @@
 -- to utilize stretch markers in editing workflow.
 
 -- Author: Simon J. Kok, simon.j.kok@gmail.com
--- Version: 1.0
+-- Version: 2.0
+
+-- Changelog --
+-- 1.0: initial release
+-- 2.0: largely rewritten, now supports multiselection
 
 function trimMediaItem(item, pos, pad)
   local delete_this = reaper.SplitMediaItem(item, pos - pad)
@@ -18,47 +22,52 @@ function trimMediaItem(item, pos, pad)
 end
 
 function convertStretchMarkersToSplits(item, pad)
-  local take = reaper.GetActiveTake(item)
-  local num_markers = reaper.GetTakeNumStretchMarkers(take)
-  
-  offset = reaper.GetMediaItemTakeInfo_Value(take, "D_STARTOFFS");
-  
-  for i = num_markers, 1, -1 do
-    local idx, pos, srcpos = reaper.GetTakeStretchMarker(take, i + 1)
+  items = {}
+  itemPos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+  take = reaper.GetActiveTake(item)
+  markerCount = reaper.GetTakeNumStretchMarkers(take)
+  for i = markerCount, 1, -1 do
+    id, pos, srcpos = reaper.GetTakeStretchMarker(take, i)
+    right = reaper.SplitMediaItem(item, itemPos + pos - pad)
     
-    reaper.SetTakeStretchMarker( take, idx, pos + pad, srcpos)
-    right = reaper.SplitMediaItem(item, pos + offset)
-    if right ~= nil then
-      reaper.SetMediaItemInfo_Value(right, "D_FADEINLEN", 0)
-      reaper.SetMediaItemInfo_Value(right, "D_FADEOUTLEN", 0)
-      local rightpos = reaper.GetMediaItemInfo_Value(right, "D_POSITION")
-      
-      reaper.SetMediaItemInfo_Value(right, "D_POSITION", rightpos - pad)
-      
-      -- After splitting, remove all other stretch markers from the new take
-      splittake = reaper.GetActiveTake(right);
-      end_srcpos = 0
-      end_pos = 0
-      for j = reaper.GetTakeNumStretchMarkers(splittake), 0, -1 do
-        local idx_j, pos_j, srcpos_j = reaper.GetTakeStretchMarker(splittake, j)
-        if(idx_j ~= idx) then
-          reaper.DeleteTakeStretchMarkers(splittake, idx_j)
-        end
-        if(idx_j == idx + 1) then
-          end_srcpos = srcpos_j
-          end_pos = pos_j
-        end
+    if(right ~= nil) then
+      splitTake = reaper.GetActiveTake(right)
+      reaper.SetMediaItemInfo_Value(right, "D_SNAPOFFSET", pad)
+      if(i < markerCount) then
+        id_n, pos_n, srcpos_n = reaper.GetTakeStretchMarker(splitTake, i + 1)
+        trimLen = srcpos_n - srcpos
+        reaper.SetMediaItemLength(right, trimLen, false)
+        
+        -- Delete all markers except at index 1
+        reaper.DeleteTakeStretchMarkers(splitTake, i + 1, markerCount)
+        reaper.DeleteTakeStretchMarkers(splitTake, 1, i - 1)
+        reaper.DeleteTakeStretchMarkers(splitTake, 0)
+        
+        table.insert(items, 1, right)
       end
-      
-      trimMediaItem(right, end_srcpos, pad)
-      reaper.DeleteTakeStretchMarkers(splittake, idx + 1)
     end
   end
   
-  reaper.DeleteTakeStretchMarkers(take, 0)
-  idx, pos, srcpos = reaper.GetTakeStretchMarker(take, 1)
-  reaper.DeleteTakeStretchMarkers(take, 1, num_markers)
-  trimMediaItem(item, srcpos, pad)
+  reaper.DeleteTakeStretchMarkers(take, 1, markerCount)
+  table.insert(items, 1, item)
+  
+  -- Trim overlaps and remove fades
+  for i = 1, #items do
+    if i < #items then
+      itemPos = reaper.GetMediaItemInfo_Value(items[i], "D_POSITION")
+      itemLen = reaper.GetMediaItemInfo_Value(items[i], "D_LENGTH")
+      rightPos = reaper.GetMediaItemInfo_Value(items[i + 1], "D_POSITION")
+      
+      if(itemPos + itemLen > rightPos) then
+        overlap = itemPos + itemLen - rightPos
+        newLen = itemLen - overlap
+        reaper.SetMediaItemLength(items[i], newLen, true)
+      end
+    end
+    
+    reaper.SetMediaItemInfo_Value(items[i], "D_FADEINLEN", 0)
+    reaper.SetMediaItemInfo_Value(items[i], "D_FADEOUTLEN", 0)
+  end
 end
 
 function matchGroup(itemA, itemB)
@@ -81,24 +90,29 @@ end
 reaper.ClearConsole();
 reaper.Undo_BeginBlock();
 
-selectedItem = reaper.GetSelectedMediaItem(0, 0)
-convertStretchMarkersToSplits(selectedItem, 0.03)
+-- In order to process multiple items at once, they must first
+-- be collected into an array, because the item index list gets
+-- regenerated every time an item is added
+local itemList = {}
 
--- Following are attempts to process multiple selected items at once,
--- either by group or by selection. Neither work and I don't know why.
 for i = 0, reaper.CountMediaItems(0) do
   local item = reaper.GetMediaItem(0, i)
+  
   if item ~= nil then
-    if(reaper.IsMediaItemSelected(item)) then
-      --reaper.ShowConsoleMsg(i .. "\n")  
-      --convertStretchMarkersToSplits(item, 0.03)
+    if reaper.IsMediaItemSelected(item) then
+      table.insert(itemList, item)
     end
-    if matchGroup(selectedItem, item) then
+    --if matchGroup(selectedItem, item) then
       --reaper.ShowConsoleMsg(selectedItemGroup .. "\n")
       --convertStretchMarkersToSplits(item, 0.03)
-    end
+    --end
   end
 end
 
+for i = 1, #itemList do
+  convertStretchMarkersToSplits(itemList[i], 0.03)
+end
+
+reaper.UpdateArrange()
 reaper.Undo_EndBlock("Convert stretch markers to splits", -1);
 
